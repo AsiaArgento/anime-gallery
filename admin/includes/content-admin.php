c27e559c23ebd9942d2245cc1fcc92d23b918b63
Matthew Marion Moncada <misakamikotocsr@gmail.com>
2021-10-11 18:24:32 -0600

    added a while loop to pass thru the User class, created the user class

--- admin/includes/content-admin.php
+++ admin/includes/content-admin.php
@@ -13,11 +13,12 @@
             <h2>Home</h2>
             <?php 
 
-            $sql = "SELECT * FROM users WHERE id=1";
-            $result = $database->query($sql);
-            $user_found = mysqli_fetch_array($result);
-            
-            echo $user_found['username'];
+            $user = new User();
+            $result_set = $user->find_all_users();
+
+            while($row = mysqli_fetch_array($result_set)) {
+                echo $row['username'] . "<br>";
+            }
             
             ?>
         </div>
---
+++ admin/includes/user.php
@@ -0,0 +1,12 @@
+<?php 
+
+class User {
+    
+    public function find_all_users() {
+        global $database;
+        $result_set = $database->query("SELECT * FROM users");
+        return $result_set;
+    }
+}
+
+?>
